b5590b47e38fff7e3fa6918bb84775cde9d8834b
Dmitriy Bulygin <128dmitriy128@gmail.com>
2020-12-04 16:47:02 +0700

    Completed task

--- client.py
+++ client.py
@@ -5,49 +5,62 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 host = "localhost"
 port = 50003
 s.connect((host, port))
-lst = [0, 0]
+lst = [0, 0, 0, 0]
 
 
-def ts():
+def power_calc():
+
    position = s.recv(1024).decode()
-
+   lst[0] = 0
    if int(position) >= -3 and int(position) <= 3:
-      lst[1] = 5
+      lst[0] = 50
+      lst[1] = 10
+      data_string = pickle.dumps(lst)
+      s.send(data_string)
+      lst[3] = lst[3] + 1
+      print(lst[0])
+   if int(position) > -5 and int(position) < -3:
+      lst[1] = 30
+      data_string = pickle.dumps(lst)
+      s.send(data_string)
+      print(lst[1])
+   if int(position) > 3 and int(position) < 5:
+      lst[1] = -30
+      data_string = pickle.dumps(lst)
+      s.send(data_string)
+      print(lst[1])
+   if int(position) > -10 and int(position) <= -5:
+      lst[1] = 60
+      data_string = pickle.dumps(lst)
+      s.send(data_string)
+      print(lst[1])
+   if int(position) >= 5 and int(position) < 10:
+      lst[1] = -60
+      data_string = pickle.dumps(lst)
+      s.send(data_string)
+      print(lst[1])
+   if int(position) > -15 and int(position) <= -10:
+      lst[1] = 100
+      data_string = pickle.dumps(lst)
+      s.send(data_string)
+      print(lst[1])
+   if int(position) >= 10 and int(position) < 15:
+      lst[1] = -100
+      data_string = pickle.dumps(lst)
+      s.send(data_string)
+      print(lst[1])
+   if (int(position) >= 15 and int(position) != 500) or int(position) <= -15:
+      lst[1] = 0
+      lst[2] = 1
+      print('Robot is dead')
       data_string = pickle.dumps(lst)
       s.send(data_string)
 
-   if int(position) > -10 and int(position) < -3:
-      lst[1] = 50
-      data_string = pickle.dumps(lst)
-      s.send(data_string)
-   if int(position) > 3 and int(position) < 10:
-      lst[1] = -50
-      data_string = pickle.dumps(lst)
-      s.send(data_string)
-   print(lst[1])
-   '''
-   else:
-      lst[1] = 5
-      data_string = pickle.dumps(lst)
-      s.send(data_string)
-'''
-   #new_data = int(data) + 2
-   #s.send(str(new_data).encode())
+   if int(position) == 500:
+      print('Mission complete!')
 
 
 while True:
-   ts()
-
+   power_calc()
 
 s.close ()
-'''
-arr = ([1,2,3,4,5,6],[1,2,3,4,5,6])
-data_string = pickle.dumps(arr)
-client.send(data_string)
-
-data = client.recv(4096)
-data_arr = pickle.loads(data)
-client.close()
-print ('Received', repr(data_arr))
-
-'''
--- main.py
+++ main.py
@@ -18,46 +18,57 @@ class client(Thread):
         Thread.__init__(self)
         self.sock = socket
         self.addr = address
-        self.position = str(random.randint(-10, 10))
+        self.position = str(random.randint(-15, 15))
         self.start()
 
     def run(self):
+        print('Current position = ', self.position)
         while 1:
             self.sock.send(self.position.encode())
             data = self.sock.recv(1024)
             data_arr = pickle.loads(data)
-            if data_arr[1] == 50:
+            if data_arr[1] == 30:
                 self.position = int(self.position) + 3
-            if data_arr[1] == -50:
+                print('Current position = ', self.position)
+            if data_arr[1] == -30:
                 self.position = int(self.position) - 3
-            if data_arr[1] == 5:
-                self.position = int(self.position) + random.randint(-7, 7)
-
+                print('Current position = ', self.position)
+            if data_arr[1] == 60:
+                self.position = int(self.position) + 6
+                print('Current position = ', self.position)
+            if data_arr[1] == -60:
+                self.position = int(self.position) - 6
+                print('Current position = ', self.position)
+            if data_arr[1] == 100:
+                self.position = int(self.position) + 10
+                print('Current position = ', self.position)
+            if data_arr[1] == -100:
+                self.position = int(self.position) - 10
+                print('Current position = ', self.position)
+            if data_arr[0] == 50:
+                self.position = int(self.position) + random.randint(-14, 14)
+                print('Current position = ', self.position)
+            if data_arr[3] > 14:
+                print('Mission complete!')
+                self.position = 500
+                self.sock.send(str(self.position).encode())
+                break
+            if data_arr[2] == 1:
+                print('Robot is dead')
+                break
             #self.position = self.position + random.randint(-3, 3)
-            print('Current position = ', self.position)
+
             self.position = str(self.position)
-            time.sleep(0.5)
+            time.sleep(0.1)
             #data = int(data) + 2
             #self.sock.send(str(data).encode())
 
 serversocket.listen(5)
-print ('server started and listening')
+print('Server started and listening')
 while 1:
     clientsocket, address = serversocket.accept()
     client(clientsocket, address)
 
 
 
-
-
-
-
-
-
-    '''
-    data = conn.recv(4096)
-    if not data: break
-    conn.send(data)
-conn.close()
-'''
 
